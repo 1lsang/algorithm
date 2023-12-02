f98a13b140eb95b7ff611bef4f76652511ada801
1lsang <ilsangpark.98@gmail.com>
2023-12-02 15:50:12 +0900

    refactor: [BOJ] 1919. 에너그램 만들기 풀이 수정

--- boj/bronze/1919.js
+++ boj/bronze/1919.js
@@ -1,23 +1,23 @@
-const [ word1, word2 ] = require('fs').readFileSync('./dev/stdin').toString().trim().split('\n');
+const [ word1, word2 ] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+function countAlphabets(word) {
+    const arr = Array.from({length: 26}, ()=>0)
+    for(let str of word) {
+        arr[str.charCodeAt()-'a'.charCodeAt()]++;
+    }
+    return arr
+}
 
 function solution(word1, word2) {
-    const arr1 = Array.from({length: 26}, ()=>0)
-    const arr2 = Array.from({length: 26}, ()=>0)
+    const arr1 = countAlphabets(word1)
+    const arr2 = countAlphabets(word2)
 
-    for(let str of word1) {
-        arr1[str.charCodeAt()-97]++;
-    }
-
-    for(let str of word2) {
-        arr2[str.charCodeAt()-97]++;
-    }
-
-    let cnt = 0;
+    let res = 0;
     for (let i=0; i<26; i++) {
-        if (arr1[i]>0 && arr2[i]>0) cnt+=Math.min(arr1[i], arr2[i]);
+        cnt += Math.abs(arr1[i]-arr2[i]);
     }
-    
-    return word1.length + word2.length - 2 * cnt;
+
+    return res;
 }
 
 console.log(solution(word1, word2));
