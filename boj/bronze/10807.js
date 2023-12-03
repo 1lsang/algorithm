aad141f1c363234158bfe52aaa2f29415e85ea99
1lsang <ilsangpark.98@gmail.com>
2023-12-03 19:38:06 +0900

    refactor: [BOJ] 10807. 개수 세기 풀이 수정

--- boj/bronze/10807.js
+++ boj/bronze/10807.js
@@ -1,14 +1,10 @@
-const [n_str, input_str, v_str] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const [n_str, input_str, v_str] = require('fs').readFileSync('./dev/stdin').toString().trim().split('\n');
 const n = Number(n_str);
 const num_list = input_str.split(' ').map(Number);
 const v = Number(v_str);
 
 function solution(n, num_list, v) {
-    let cnt = 0;
-    num_list.forEach((num)=>{
-        if (num===v) cnt++;
-    })
-    return cnt
+    return num_list.filter(num=>num===v).length;
 }
 
 console.log(solution(n, num_list, v))
