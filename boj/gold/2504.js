5c9fa0eff6ca4127cd636b5218e85a23f83e09a5
1lsang <ilsangpark.98@gmail.com>
2023-12-13 13:50:18 +0900

    refactor: [BOJ] 2504. 괄호의 값 풀이 수정

--- boj/gold/2504.js
+++ boj/gold/2504.js
@@ -3,51 +3,31 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim();
 
 function solution(input) {
+    const numBracket = { '(': 2, ')':2, '[': 3, ']': 3 };
+    const matchBracket = (b) => (b===')') ? '(' : '[';
     const s = [];
-
-    // 올바르지 못한 괄호열 체크
-    for (let str of input) {
-        if (str === '(' || str === '[') s.push(str);
-        else if (str === ')' && s.at(-1) === '(') s.pop();
-        else if (str === ']' && s.at(-1) === '[') s.pop();
-        else return 0;
-    }
-    if (s.length!==0) return 0;
+    let answer = 0; // 누적 합
+    let num = 1;    // 곱해질 수
+    let prev = '';
 
     // 계산
     for (let str of input) {
         if (str === '(' || str === '[') {
             s.push(str);
+            num *= numBracket[str];     // 여는 괄호가 들어올 경우 곱해질 수 증가
         }
-        else if (str === ')') {
-            if (s.at(-1)==='(') {
-                s.pop();
-                s.push(2);
-            } else {
-                let tmp = 0;
-                while (s.at(-1)!=='(') {
-                    tmp += s.pop();
-                }
-                s.pop();
-                s.push(tmp*2);
-            }
+        else if (str === ')' || str === ']') {
+            // 올바르지 않은 괄호열
+            if (s.at(-1) !== matchBracket(str)) return 0;     
+            // 바로 이전 단계가 왼쪽 괄호일 때 누적  
+            if (prev === matchBracket(str)) answer += num;    
+            s.pop();
+            num /= numBracket[str];
         }
-        else if (str === ']') {
-            if (s.at(-1)==='[') {
-                s.pop();
-                s.push(3);
-            } else {
-                let tmp = 0;
-                while (s.at(-1)!=='[') {
-                    tmp += s.pop();
-                }
-                s.pop();
-                s.push(tmp*3);
-            }
-        }
+        prev = str;
     }
 
-    return s.reduce((a,b)=>a+b, 0);
+    return s.length === 0 ? answer : 0;
 }
 
 console.log(solution(input));
