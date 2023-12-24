2703ee52974b123e9184b383459e03dd5bd9b253
1lsang <ilsangpark.98@gmail.com>
2023-12-24 23:42:04 +0900

    solve: [BOJ] 15666. N과 M(12)

---
+++ boj/silver/15666.js
@@ -0,0 +1,28 @@
+"use strict"
+
+const [[n, m], arr] = require('fs').readFileSync('./dev/stdin').toString().trim().split('\n').map(str => str.split(' ').map(Number));
+
+function solution(n, m, arr) {
+    const answers = [];
+    const answer = [];
+    const sorted = arr.sort((a, b) => a - b);
+
+    function bt(k) {
+        if (k === m) return answers.push(answer.join(' '));
+        let tmp = 0;
+        for (let i = 0; i < n; i++) {
+            if (tmp !== sorted[i] && (answer.length === 0 || answer.at(-1) <= sorted[i])) {
+                answer.push(sorted[i]);
+                tmp = sorted[i];
+                bt(k+1);
+                answer.pop();
+            }
+        }
+    }
+
+    bt(0);
+
+    return answers.join('\n');
+}
+
+console.log(solution(n, m, arr));
