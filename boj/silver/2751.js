c680213936969e280a51327ea2e14351228bc3f7
1lsang <ilsangpark.98@gmail.com>
2024-01-03 19:54:18 +0900

    solve: [BOJ] 2751. 수 정렬하기 2

---
+++ boj/silver/2751.js
@@ -0,0 +1,35 @@
+const [n, ...arr] = require('fs').readFileSync('./dev/stdin').toString().trim().split('\n').map(Number);
+
+function solution(n, arr) {
+    function merge(start, end) {
+        const tmp = [];
+        const mid = Math.floor((start + end) / 2);
+        let idx1 = start;
+        let idx2 = mid;
+        for (let i = start; i < end; i++) {
+            if (idx1 === mid) tmp.push(arr[idx2++]);
+            else if (idx2 === end) tmp.push(arr[idx1++]);
+            else if (arr[idx1] <= arr[idx2]) tmp.push(arr[idx1++]);
+            else tmp.push(arr[idx2++]);
+        }
+        for (let i = 0; i < tmp.length; i++) {
+            arr[start + i] = tmp[i];
+        }
+    }
+    
+    function mergeSort(start, end) {
+        // 1. 배열의 길이가 1이 될 때까지 쪼갠다. 
+        if (end === start + 1) return;
+        const mid = Math.floor((start + end) / 2);
+        mergeSort(start, mid);
+        mergeSort(mid, end);
+        // 2. 배열의 길이가 1이 되면 쪼갠 배열들을 합친다. 
+        merge(start, end);
+    }
+
+    mergeSort(0, n);
+
+    return arr.join('\n');
+}
+
+console.log(solution(n, arr));
