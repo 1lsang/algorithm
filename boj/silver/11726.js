b46a965f15fa11dfa496f3fc90a89070746b94fa
1lsang <ilsangpark.98@gmail.com>
2024-01-09 17:27:46 +0900

    refactor: [BOJ] 11726. 2xn 타일링 풀이 수정

--- boj/silver/11726.js
+++ boj/silver/11726.js
@@ -3,21 +3,24 @@
 const n = Number(require('fs').readFileSync('./dev/stdin').toString().trim());
 
 function solution(n) {
+    const mod = 10007;
+
     // 1. 배열: d[k] = 2*k 크기의 직사각형을 채우는 방법
     const d = Array.from({ length: n+1 }, () => 0);
     
+
     // 2. 점화식
     // d[k] = d[k-1] + d[k-2]
 
     // 3. 초기값
-    d[1] = 1n;
-    d[2] = 2n;
+    d[1] = 1;
+    d[2] = 2;
 
     for (let k = 3; k <= n; k++) {
-        d[k] = d[k-1] + d[k-2]
+        d[k] = d[k-1] % mod + d[k-2] % mod;
     }
 
-    return (d[n] % 10007n).toString();
+    return d[n] % mod;
 }
 
 console.log(solution(n));
