3b1ae53d7a0d8bb3314b87f72fbbc930f07d693c
1lsang <ilsangpark.98@gmail.com>
2024-01-22 22:23:05 +0900

    solve: [BOJ] 1330. 두 수 비교하기

---
+++ boj/bronze/1330.js
@@ -0,0 +1,11 @@
+"use strict"
+
+const [a, b] = require('fs').readFileSync('./dev/stdin').toString().trim().split(' ').map(Number);
+
+function solution(a, b) {
+    if (a > b) return '>';
+    else if (a < b) return '<';
+    else return '==';
+}
+
+console.log(solution(a, b));
